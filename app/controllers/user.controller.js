4792eafc09ee4e2ada02ea8b16de572697026601
Davide Berardi <davidebrr@gmail.com>
2016-08-01 10:28:38 +0200

    add basic user RESTful controller

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -4,9 +4,9 @@ module.exports = function(User){
 
 		getAll: function(req, res) {
 			User.find({}, function(err, users) {
-				if(!err) { 
+				if(!err) {
 
-					res.json({data:users}); 
+					res.json({data:users});
 
 				}else{
 
@@ -33,20 +33,74 @@ module.exports = function(User){
 		},
 
 		update: function(req, res){
+			User.update({"_id":req.params.id}, req.body.user ,function(err, user) {
+				if(!err) {
 
-			res.sendStatus(200);
+
+					res.json({data:user});
+				}else{
+					res.json({error:err});
+				}
+			});
 
 		},
 
 		delete: function(req, res){
+			if(req.user._id === req.params.id){
 
-			res.sendStatus(200);
+				User.find({"_id":req.params.id}, function(err, user) {
 
+					if(!err) {
+
+						if(!user){
+
+							res.json({
+								error:{
+									type:'DBException',
+									message:'no user found'
+								}
+							});
+
+						}else{
+
+							res.json({data:"User deleted successfully"});
+						}
+					}else{
+
+						res.json({error:err});
+
+					}
+				});
+				res.sendStatus(200);
+
+			}
 		},
 
 		create: function(req, res){
 
-			res.sendStatus(200);
+			var new_user = new User();
+
+			new_user.name = req.body.user.name;
+			new_user.password = req.body.user.password;
+			new_user.email = req.body.user.email;
+			new_user.admin = false;
+
+			new_user.save(function(err,saved){
+
+				if(!err){
+
+					res.send({data:"New User Created"});
+
+				}else{
+
+					res.send({
+						error:{
+							type:"DBException",
+							message:err
+						}
+					});
+				}
+			});
 
 		}
 
--- app/models/user.model.js
+++ app/models/user.model.js
@@ -5,37 +5,46 @@ var config   = require('../config/config');
 
 var UserSchema = mongoose.Schema({
 	name: { type:String, require:true, unique: true },
-	password: { type:String, require:true },
+	email: { type:String, require:true, unique: true },
+	password: { type:String, require:true,select: false },
 	admin: { type:Boolean, default: false }
 });
 
 UserSchema.pre('save', function (next) {
-  var user = this;
-  if(user.isModified('password')){
-    user.password = bcrypt.hashSync(user.password);
-  }
-  next();
+
+	var user = this;
+
+	if(user.isModified('password')){
+		user.password = bcrypt.hashSync(user.password);
+	}
+
+	next();
 });
 
 UserSchema.methods.checkPassword = function (hash, done) {
-  var {username, password} = this;
-  bcrypt.compare(hash, password, function (err, res) {
-    if(res) return done(null, true );
-    return done('Wrong Password!');
-  });
+
+	var {username, password} = this;
+
+	bcrypt.compare(hash, password, function (err, res) {
+		if(res) return done(null, true );
+		return done('Wrong Password!');
+	});
 };
 
 UserSchema.methods.auth = function (done) {
-  var user = this;
-  var token = jwt.sign({
-    name: user.name,
-    admin: user.admin
-  }, config.secret, {expiresIn: 86400 });
-  done(null, {
-    expire: Date.now() + 86400,
-    message: 'Authorization token',
-    token: token
-  });
+
+	var user = this;
+
+	var token = jwt.sign({
+		name: user.name,
+		admin: user.admin
+	}, config.secret, {expiresIn: 86400 });
+
+	done(null, {
+		expire: Date.now() + 86400,
+		message: 'Authorization token',
+		token: token
+	});
 };
 
 
