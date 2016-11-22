de9f1b21d847ebe470b65d795bd0997596bddbec
Davide Berardi <davidebrr@gmail.com>
2016-11-22 11:08:52 +0000

    update user model

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -5,47 +5,43 @@ var config   = require('../config/config');
 
 var UserSchema = mongoose.Schema({
 	username: { type:String, require:true, unique: true },
-	email: { type:String, require:true, unique: true },
-	password: { type:String, require:true,select: false },
-	admin: { type:Boolean, default: false }
+  email:{ type:String, require:true, unique: true },
+	password: { type:String, require:true },
+  admin: { type:Boolean, default: false },
+  company: {type:String},
+  website: {type:String},
+  phone: {type:String}
 });
 
 UserSchema.pre('save', function (next) {
-
-	var user = this;
-
-	if(user.isModified('password')){
-		user.password = bcrypt.hashSync(user.password);
-	}
-
-	next();
+  var user = this;
+  if(user.isModified('password')){
+    user.password = bcrypt.hashSync(user.password);
+  }
+  next();
 });
 
 UserSchema.methods.checkPassword = function (hash, done) {
-
-	var {username, password} = this;
-
-	bcrypt.compare(hash, password, function (err, res) {
-		if(res) return done(null, true );
-		return done('Wrong Password!');
-	});
-};
+  var {username, password} = this;
+  bcrypt.compare(hash, password, function (err, res) {
+    if(res) return done(null, true );
+    return done('Wrong Password!');
+  });
+}
 
 UserSchema.methods.auth = function (done) {
-
-	var user = this;
-
-	var token = jwt.sign({
-		username: user.username,
-		admin: user.admin
-	}, config.secret, {expiresIn: 86400 });
-
-	done(null, {
-		expire: Date.now() + 86400,
-		message: 'Authorization token',
-		token: token
-	});
-};
+  var usr = this;
+  var token = jwt.sign({
+    name: usr.username,
+    _id: usr._id,
+    admin: usr.admin
+  }, config.secret, {expiresIn: 86400 });
+  done(null, {
+    expire: Date.now() + 86400,
+    message: 'Authorization token',
+    token: token
+  });
+}
 
 
 module.exports = mongoose.model('User', UserSchema, 'users');
