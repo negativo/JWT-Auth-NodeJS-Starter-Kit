cf7d05ad1dc9f33fd085b136d9773bca4c9dc4a5
Davide Berardi <davide.berardi@tomtom.com>
2020-01-13 11:54:37 +0100

    fix mongodb address issue

--- app/config/database.js
+++ app/config/database.js
@@ -5,32 +5,32 @@ mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 
 module.exports = {
-	connect: () => {
+  connect: () => {
 
-		db.on('error',
-			console.error.bind(console, 'MongoDB Connection Error. Please make sure that MongoDB is running.')
-		);
+    db.on('error',
+      console.error.bind(console, 'MongoDB Connection Error. Please make sure that MongoDB is running.')
+    );
 
-		db.once('open', () => {
+    db.once('open', () => {
 
-			process.emit('event:mongodb_connected');
+      process.emit('event:mongodb_connected');
 
-		});
+    });
 
-		if(process.env.NODE_ENV === 'TEST'){
+    if(process.env.NODE_ENV === 'TEST'){
 
-			mongoose.connect(config.test_database);
+      mongoose.connect(config.test_database);
 
-		} else {
+    } else {
 
-			mongoose.connect(config.database);
+      mongoose.connect(config.database);
 
-		}
+    }
 
-		return db;
-	},
-	info: () => {
-		//  MongoDB status (0 = disconnected; 1 = connected; 2 = connecting; 3 = disconnecting)
-		return db.readyState;
-	}
+    return db;
+  },
+  info: () => {
+    //  MongoDB status (0 = disconnected; 1 = connected; 2 = connecting; 3 = disconnecting)
+    return db.readyState;
+  }
 }
--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -1,42 +1,42 @@
 module.exports = function(User){
 
-	return {
+  return {
 
-		setup:function(req, res) {
-			var nick = new User({
-				email: 'davidebrr+localdev@gmail.com',
-				username: 'user',
-				password: 'password',
-				admin: true
-			});
-			nick.save(function(err) {
-				if (err) throw err;
-				console.log('User saved successfully');
-				return res.json({ success: true });
-			});
-		},
-		index: function(req, res) {
-			return res.send('API  http://localhost:'+ process.env.PORT +'/api');
-		},
-		apiIndex:function(req, res) {
-			return res.json({ message: 'WIT Dashboard API' });
+    setup:function(req, res) {
+      var nick = new User({
+        email: 'davidebrr+localdev@gmail.com',
+        username: 'user',
+        password: 'password',
+        admin: true
+      });
+      nick.save(function(err) {
+        if (err) throw err;
+        console.log('User saved successfully');
+        return res.json({ success: true });
+      });
+    },
+    index: function(req, res) {
+      return res.send('API  http://localhost:'+ process.env.PORT +'/api');
+    },
+    apiIndex:function(req, res) {
+      return res.json({ message: 'WIT Dashboard API' });
     },
 
-		check:function(req, res) {
+    check:function(req, res) {
 
-			return res.json({
-				success:true,
-				user: req.user,
-				message: 'Normal user accesible route'
-			});
-		},
-		adminCheck:function(req, res) {
-			return res.json({
-				success:true,
-				user: req.user,
-				message: 'Admin user accesible route'
-			});
-		}
-	};
+      return res.json({
+        success:true,
+        user: req.user,
+        message: 'Normal user accesible route'
+      });
+    },
+    adminCheck:function(req, res) {
+      return res.json({
+        success:true,
+        user: req.user,
+        message: 'Admin user accesible route'
+      });
+    }
+  };
 
 };
